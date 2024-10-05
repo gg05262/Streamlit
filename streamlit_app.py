a2190b989d8e2d5b033b86523a3edbc2117fc1cd
gg05262 <128438533+gg05262@users.noreply.github.com>
2024-10-05 17:30:45 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,51 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
+import numpy as np
+import matplotlib.pyplot as plt # 시각화 라이브러리
 
-st.title('Streamlit for sin and cos function')
 
-x_start = st.slider('x 시작값', 0.0,10.0,0.0)
-x_end = st.slider('x 종료값', 10.0, 20.0, 10.0)
+st.title('Streamlit for sin and cos fuctioni visualiztion')
 
-x = np.linspace(x_start,x_end)
+x_start = st.slider('x 시작값' ,  0.0, 10.0, 0.0)
+x_end = st.slider('x 시작값' ,  10.0, 20.0, 10.0)
+
+
+x = np.linspace(x_start, x_end)
 
 y_sin = np.sin(x)
 y_cos = np.cos(x)
 
-fig, ax = plt.subplots()
 
-ax.plot(x,y_sin)
-ax.plot(x,y_cos)
-ax.legend(['sin','cos'])
-ax.set_xlabel(['X-axis'])
-ax.set_ylabel(['Y-axis'])
+fig , ax = plt.subplots()
 
-ax.set_title('sin and cos function')
+ax.plot(x, y_sin)  
+ax.plot(x, y_cos)
+ax.legend(['sin', 'cos'])
+ax.set_xlabel('X-axis')
+ax.set_ylabel('Y-axis')
 
-@st.cache
-def expensive_computation(x):
-  return np.sin(x) + np.cos(x)
-
-result = expensive_computation(x)
-
-
-df = px.data.gapminder().query("continent == Oceania")
-fig = px.bar(df, x='year', y='pop')
-
-
+ax.set_title('sin and cos fuction')
 
 st.pyplot(fig)
+
+@st.cache
+def expensive_computataion(x):
+    return np.sin(x) + np.cos(x)
+
+
+
+result = expensive_computataion(x)
+
+
+
+import plotly.express as px
+df = px.data.gapminder().query("continent == 'Oceania'")
+df
+
+fig = px.bar(df, x='year' , y='pop')
+st.plotly_chart(fig)
+
+
+df1 = px.data.gapminder().query("year == 2007")
+fig1 = px.treemap(df1, path=[px.Constant('World'), 'continent','country'], values = 'pop' , color = 'lifeExp')
+st.plotly_chart(fig1)
